21f0930bdfc566a3dc120e86aba536c194771e25
Noomi <kykkanenn@gmail.com>
2024-02-21 15:28:12 +0200

    Part 01 valmis

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+const Header = ({ course }) => <h1>{course}</h1>;
+
+const Content = ({ parts }) => (
+  <div>
+    {parts.map((part, index) => (
+      <p key={index}>
+        {part.name} {part.exercises}
+      </p>
+    ))}
+  </div>
+);
+
+const Total = ({ parts }) => (
+  <p>
+    Number of exercises {parts.reduce((sum, part) => sum + part.exercises, 0)}
+  </p>
+);
+
+const App = () => {
+  const course = "Half Stack application development";
+  const parts = [
+    { name: "Fundamentals of React", exercises: 10 },
+    { name: "Using props to pass data", exercises: 7 },
+    { name: "State of a component", exercises: 14 },
+  ];
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
+    </div>
+  );
+};
+
+export default App;
